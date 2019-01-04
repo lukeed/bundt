8ebce079331d74158b2a738819f1dab9bfd404b8
Luke Edwards <luke.edwards05@gmail.com>
2019-01-04 11:23:22 -0800

    feat: add help text thru `-h|--help` flags

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const { gzipSync } = require('zlib');
 const { existsSync, readFileSync, writeFileSync } = require('fs');
 const { dirname, normalize, resolve } = require('path');
 const { white, cyan, dim } = require('kleur');
-const imports = require('rewrite-imports');
-const { minify } = require('terser');
-const mkdir = require('mk-dirs');
 
 const _ = ' ';
 const gutter = _.repeat(4);
@@ -12,6 +9,8 @@ const UNITS = ['B ', 'kB', 'MB', 'GB'];
 const lpad = (str, max) => _.repeat(max - str.length) + str;
 const rpad = (str, max) => str + _.repeat(max - str.length);
 const th = dim().bold().italic().underline;
+const filename = white().underline;
+const bullet = white().bold;
 
 function size(val=0) {
 	if (val < 1e3) return `${val} ${UNITS[0]}`;
@@ -36,6 +35,22 @@ function write(file, data, isUMD) {
 	});
 }
 
+function help() {
+	let msg = '';
+	msg += '\n  Usage\n    $ bundt [entry]\n';
+	msg += `\n  Config\n    If no ${dim('[entry]')} was provided, then ${filename('src/index.js')} is used.`;
+	msg += `\n    Configuration lives within your ${filename('package.json')} file as these keys:`;
+	msg += `\n      • ${bullet('"main"')} – the output path for your CommonJS file ${dim().italic(`(default: ${filename(`dist/${pkg.name}.js`)})`)}`;
+	msg += `\n      • ${bullet('"module"')} – the output path for your ES Module file ${dim().red().italic('not built if undefined')}`;
+	msg += `\n      • ${bullet('"unpkg"')} ${dim('or')} ${bullet('"umd:main"')} – the output path for your UMD file ${dim().red().italic('not built if undefined')}`;
+	msg += `\n      • ${bullet('"umd:name"')} – the name of your UMD factory ${dim().italic(`(default: "${pkg.name}")`)}`;
+	msg += `\n      • ${bullet('"terser"')} – a config object to customize Terser behavior`;
+	msg += `\n    You may use a ${filename('.terserrc')} file to store configuration instead of the ${bullet('"terser"')} key.\n`;
+	msg += '\n  Options\n    -h, --help    Displays this message\n';
+	msg += '\n  Examples\n    $ bundt\n    $ bundt lib/index.js\n';
+	return console.log(msg);
+}
+
 const rcfile = resolve('.terserrc');
 const pkgfile = resolve('package.json');
 const pkg = existsSync(pkgfile) && require(pkgfile);
@@ -43,7 +58,13 @@ if (!pkg) return console.log('Does not exist: %s', pkgfile);
 
 const argv = process.argv.slice(2);
 const entry = resolve(argv[0] || 'src/index.js');
+if (argv.includes('-h') || argv.includes('--help')) return help();
 if (!existsSync(entry)) return console.log('Does not exist: %s', entry);
+
+// We'll actually do something – require deps
+const imports = require('rewrite-imports');
+const { minify } = require('terser');
+const mkdir = require('mk-dirs');
 
 // Parsed config
 const output = {
