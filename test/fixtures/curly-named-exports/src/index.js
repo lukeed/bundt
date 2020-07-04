5f4614d2958eea0c011b1620db489c3f37c8f66c
Daniel Fischer <daniel@d-fischer.dev>
2020-07-04 12:10:55 +0200

    add tests

---
+++ test/fixtures/curly-named-exports/src/index.js
@@ -0,0 +1,19 @@
+function foo(a) {
+	return a + a;
+}
+
+const bar = b => b + b;
+
+async function baz(a) {
+	return a + a;
+}
+
+let hello = 'world';
+
+var abc = 123;
+
+class Foo extends Component {
+	//
+}
+
+export { foo, bar, baz, hello, abc, Foo };
